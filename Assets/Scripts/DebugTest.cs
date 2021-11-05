ed00dc6fef1c78ea9a51576763461a275620d8ef
willi bücking <willi.buecking@students.hs-mainz.de>
2021-11-05 15:07:44 +0100

    add counter for debug-messages

--- Assets/Scripts/DebugTest.cs
+++ Assets/Scripts/DebugTest.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class DebugTest : MonoBehaviour
 {
     // Start is called before the first frame update
+               int x = 2;
     void Start()
     {
+    
         Debug.Log("Hello World");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("fuck, this is too much");
+        Debug.Log("fuck, this is debugmessage number");
+        Debug.Log(x);
+        x = x + 1;
     }
 }
