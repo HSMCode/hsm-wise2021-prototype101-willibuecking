bda9c1f99a250e28bd18c31ce638b17acd937eac
willi bücking <willi.buecking@students.hs-mainz.de>
2021-11-19 10:32:28 +0100

    improve efficiency and clarity of the NumberCalcs-script.

--- Assets/Scripts/NumberCalcs.cs
+++ Assets/Scripts/NumberCalcs.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class NumberCalcs : MonoBehaviour
 {   
 
-    public int Integer1 = 3;    //create 5 wholenumber-variables
-    public int Integer2 = 7;
-    public int Integer3 = 11;
-    public int Integer4 = 6;
-    public int Integer5 = 25;
-    public float Float1;        //This will be used for calculations later on
-    public string Ansage = "Die Summe der ersten drei Variablen dieses Scripts geteilt durch drei ist ";    //This will be some text to make the console-outpur understandable
+    public int[] myIntegers =  new int[5] { 3, 7, 11, 6, 25 };  //create 5 wholenumber-variables
+
+    public int sum = 0;
+
+    public float result;        //This will be used for calculations later on
+    public string ansage = "Die Summe der ersten fünf Variablen dieses Scripts geteilt durch drei ist ";    //This will be some text to make the console-outpur understandable
 
     // Start is called before the first frame update
     void Start()
     {
+        foreach (int i in myIntegers)
+        {
+            sum += i;
+        }
 
-        Float1 = (Integer1 + Integer2 + Integer3 + Integer4 + Integer5)/3;    //calculations (sum of three integers divided by 3) 
-        Debug.Log(Ansage + Float1);                     //finally print the result
+        result = sum/3;     //calculations (sum of 5 integers divided by 3)
+ 
+        Debug.Log(ansage + result);                     //finally print the result
 
    
     }
